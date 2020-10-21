156bf756cb04439e6893d04442f5fb2225e6433f
oekshido <joseugarte2262005@gmail.com>
2020-10-21 18:07:53 -0300

    First usable site

--- index.js
+++ index.js
@@ -1,27 +1,48 @@
 let styleRoot = document.documentElement.style
+let terminalElement = document.getElementById("terminal")
 
+// Base definitions
 let colors = {
-	black: "000000",
-	white: "bbbbbb",
-	red: "bb0000",
-	green: "00bb00",
-	blue: "0000bb",
-	cyan: "00bbbb",
-	magenta: "bb00bb",
-	yellow: "bbbb00",
-	brightblack: "000000",
-	brightwhite: "ffffff",
-	brightred: "ff0000",
-	brightgreen: "00ff00",
-	brightblue: "0000ff",
-	brightcyan: "00ffff",
-	brightmagenta: "ff00ff",
-	brightyellow: "ffff00",
+	black: "#000000",
+	white: "#bbbbbb",
+	red: "#bb0000",
+	green: "#00bb00",
+	blue: "#0000bb",
+	cyan: "#00bbbb",
+	magenta: "#bb00bb",
+	yellow: "#bbbb00",
+	brightblack: "#000000",
+	brightwhite: "#ffffff",
+	brightred: "#ff0000",
+	brightgreen: "#00ff00",
+	brightblue: "#0000ff",
+	brightcyan: "#00ffff",
+	brightmagenta: "#ff00ff",
+	brightyellow: "#ffff00",
+	// Syntax rules
+	bg: "black",
+	fg: "white",
+	num: "blue",
+	str: "red",
+	esc: "magenta",
+	ppc: "magenta",
+	pps: "red",
+	opt: "brightwhite",
+	slc: "brightblack",
+	kwa: "yellow",
+	kwb: "brightyellow",
+	kwc: "cyan",
+	kwd: "green"
 }
 
+function parse(str) {
+	return (str[0] == "#") ? str : `var(--${str})`
+}
+
+// Update color values
 for(let element of document.getElementsByTagName("input")) {
 	element.value = colors[element.id] // Set default value
-	styleRoot.setProperty("--" + element.id, "#" + element.value)
+	styleRoot.setProperty("--" + element.id, parse(element.value))
 	element.insertAdjacentHTML(
 		"afterend",
 		`<div style="background-color: var(--${element.id});` +
@@ -30,14 +51,14 @@ for(let element of document.getElementsByTagName("input")) {
 
 	element.addEventListener("focusout", () => {
 		colors[element.id] = element.value
-		styleRoot.setProperty("--" + element.id, "#" + element.value)
+		styleRoot.setProperty("--" + element.id, parse(element.value))
 	})
 }
 
-// syntax highlightinh
+// syntax highlighting
 let css = ""
-for(let color of Object.keys(colors)) {
-	css += `#terminal .${color} {
+for(let [color, value] of Object.entries(colors)) {
+	css += `.hl .${color} {
 		color: var(--${color});
 	}\n`
 }
@@ -45,11 +66,10 @@ css = `<style>${css}</style>`
 
 document.getElementsByTagName("link")[0].insertAdjacentHTML("afterend", css)
 
-
-let terminalElement = document.getElementById("terminal")
-
 // unfocus on Enter key
 document.addEventListener("keydown", (e) => {
 	if(e.key == "Enter")
 		e.target.blur()
 })
+
+terminalElement.innerHTML = samples["c"]
--- samples.js
+++ samples.js
@@ -1,97 +1,84 @@
 let samples = {
-	"c": `<?xml version="1.0" encoding="UTF-8" ?>
-	<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-	<!-- This file was created with the aha Ansi HTML Adapter. https://github.com/theZiz/aha -->
-	<html xmlns="http://www.w3.org/1999/xhtml">
-	<head>
-	<meta http-equiv="Content-Type" content="application/xml+xhtml; charset=UTF-8"/>
-	<title>stdin</title>
-	</head>
-	<body>
-	<pre>
-	<span class="magenta">#include</span> <span class="red">&quot;mmenu.h&quot;</span><span class="magenta"></span>
-	<span class="magenta">#include &lt;stdio.h&gt;</span>
-	<span class="magenta">#include &lt;stdlib.h&gt;</span>
-	
-	<span class="magenta">#define MAX_STR_SIZE 128</span>
-	<span class="magenta">#define D_SIZE 128</span>
-	<span class="magenta">#define PATH</span> <span class="red">&quot;/tmp/mmenu&quot;</span><span class="magenta"></span>
-	
-	<span class="green">typedef</span> <span class="green">struct</span> darr {
-			<span class="green">char</span>** strs;
-			<span class="green">int</span> size;
-			<span class="green">int</span> used;
-	} darr;
-	
-	<span class="green">void</span> <span class="green">darr_push</span>(darr* arr, <span class="green">char</span>* str) {
-			<span class="yellow">if</span>(arr-&gt;used == arr-&gt;size) {
-					arr-&gt;size *= <span class="blue">2</span>;
-					arr-&gt;strs = <span class="green">realloc</span>(arr-&gt;strs, arr-&gt;size * <span class="yellow">sizeof</span>(<span class="green">char</span>*));
-					<span class="yellow">if</span>(arr-&gt;strs == NULL) <span class="green">exit</span>(<span class="blue">1</span>);
-			}
-			arr-&gt;strs[arr-&gt;used] = str;
-			arr-&gt;used++;
-	}
-	
-	<span class="green">void</span> <span class="green">darr_free</span>(darr* arr) {
-			<span class="yellow">for</span>(<span class="green">int</span> i = <span class="blue">0</span>; i &lt; arr-&gt;used; i++) {
-					<span class="green">free</span>(arr-&gt;strs[i]);
-			}
-			<span class="green">free</span>(arr-&gt;strs);
-			<span class="green">free</span>(arr);
-	}
-	
-	darr* <span class="green">get_options</span>(<span class="green">FILE</span>* file_ptr) {
-			darr* options = <span class="green">malloc</span>(<span class="yellow">sizeof</span>(darr));
-			<span class="yellow">if</span>(options == NULL) <span class="green">exit</span>(<span class="blue">1</span>);
-			options-&gt;strs = <span class="green">malloc</span>(D_SIZE * <span class="yellow">sizeof</span>(<span class="green">char</span>*));
-			<span class="yellow">if</span>(options-&gt;strs == NULL) <span class="green">exit</span>(<span class="blue">1</span>);
-			options-&gt;size = D_SIZE;
-			options-&gt;used = <span class="blue">0</span>;
-	
-			<span class="green">char</span> chr;
-			<span class="green">char</span>* str;
-			<span class="green">int</span> i;
-			<span class="yellow">do</span> {
-					str = <span class="green">malloc</span>(MAX_STR_SIZE * <span class="yellow">sizeof</span>(<span class="green">char</span>));
-					<span class="yellow">if</span>(str == NULL) <span class="green">exit</span>(<span class="blue">1</span>);
-					<span class="yellow">for</span>(i = <span class="blue">0</span>; i &lt; MAX_STR_SIZE; i++) {
-							chr = <span class="green">fgetc</span>(file_ptr);
-							<span class="yellow">if</span>(chr == <span class="red">'</span><span class="magenta">\\n</span><span class="red">'</span> || chr == EOF) <span class="yellow">break</span>;
-							str[i] = chr;
-					}
-					<span class="green">darr_push</span>(options, str);
-			}
-			<span class="yellow">while</span>(chr != EOF);
-	
-			<span class="yellow">return</span> options;
-	}
-	
-	<span class="green">int</span> <span class="green">main</span>(<span class="green">int</span> argc, <span class="green">char</span>** argv) {
-			<span class="green">FILE</span>* file_ptr = <span class="green">fopen</span>(PATH, <span class="red">&quot;r&quot;</span>);
-			<span class="yellow">if</span>(file_ptr == NULL) <span class="green">exit</span>(<span class="blue">1</span>);
-			darr* options = <span class="green">get_options</span>(file_ptr);
-			<span class="green">fclose</span>(file_ptr);
-			file_ptr = <span class="green">fopen</span>(PATH, <span class="red">&quot;w&quot;</span>);
-			<span class="yellow">if</span>(file_ptr == NULL) <span class="green">exit</span>(<span class="blue">1</span>);
-	
-			<span class="green">int</span> chosen;
-			<span class="yellow">if</span>(argc == <span class="blue">1</span>)
-					chosen = <span class="green">mmenu</span>(options-&gt;strs, options-&gt;used, <span class="red">&quot;&gt; &quot;</span>);
-			<span class="yellow">else</span>
-					chosen = <span class="green">mmenu</span>(options-&gt;strs, options-&gt;used, argv[<span class="blue">1</span>]);
-	
-			<span class="yellow">if</span>(chosen == -<span class="blue">1</span>)
-					<span class="green">fputs</span>(<span class="red">&quot;&quot;</span>, file_ptr);
-			<span class="yellow">else if</span>(argc &gt; <span class="blue">2</span> &amp;&amp; argv[<span class="blue">2</span>][<span class="blue">0</span>] == <span class="red">'t'</span>)
-					<span class="green">fprintf</span>(file_ptr, <span class="red">&quot;%i&quot;</span>, chosen);
-			<span class="yellow">else</span>
-					<span class="green">fputs</span>(options-&gt;strs[chosen], file_ptr);
-	
-			<span class="green">darr_free</span>(options);
-			<span class="green">fclose</span>(file_ptr);
-	}
-	</pre>
-	</body>
-	</html>`
+	"c": `<pre class="hl"><span class="hl ppc">#include</span> <span class="hl pps">&quot;mmenu.h&quot;</span><span class="hl ppc"></span>
+<span class="hl ppc">#include &lt;stdio.h&gt;</span>
+<span class="hl ppc">#include &lt;stdlib.h&gt;</span>
+
+<span class="hl ppc">#define MAX_STR_SIZE 128</span>
+<span class="hl ppc">#define D_SIZE 128</span>
+<span class="hl ppc">#define PATH</span> <span class="hl pps">&quot;/tmp/mmenu&quot;</span><span class="hl ppc"></span>
+
+<span class="hl kwc">typedef</span> <span class="hl kwb">struct</span> darr <span class="hl opt">{</span>
+\t<span class="hl kwb">char</span><span class="hl opt">**</span> strs<span class="hl opt">;</span>
+\t<span class="hl kwb">int</span> size<span class="hl opt">;</span>
+\t<span class="hl kwb">int</span> used<span class="hl opt">;</span>
+<span class="hl opt">}</span> darr<span class="hl opt">;</span>
+
+<span class="hl kwb">void</span> <span class="hl kwd">darr_push</span><span class="hl opt">(</span>darr<span class="hl opt">*</span> arr<span class="hl opt">,</span> <span class="hl kwb">char</span><span class="hl opt">*</span> str<span class="hl opt">) {</span>
+\t<span class="hl kwa">if</span><span class="hl opt">(</span>arr<span class="hl opt">-&gt;</span>used <span class="hl opt">==</span> arr<span class="hl opt">-&gt;</span>size<span class="hl opt">) {</span>
+\t\tarr<span class="hl opt">-&gt;</span>size <span class="hl opt">*=</span> <span class="hl num">2</span><span class="hl opt">;</span>
+\t\tarr<span class="hl opt">-&gt;</span>strs <span class="hl opt">=</span> <span class="hl kwd">realloc</span><span class="hl opt">(</span>arr<span class="hl opt">-&gt;</span>strs<span class="hl opt">,</span> arr<span class="hl opt">-&gt;</span>size <span class="hl opt">*</span> <span class="hl kwa">sizeof</span><span class="hl opt">(</span><span class="hl kwb">char</span><span class="hl opt">*));</span>
+\t\t<span class="hl kwa">if</span><span class="hl opt">(</span>arr<span class="hl opt">-&gt;</span>strs <span class="hl opt">==</span> NULL<span class="hl opt">)</span> <span class="hl kwd">exit</span><span class="hl opt">(</span><span class="hl num">1</span><span class="hl opt">);</span>
+\t<span class="hl opt">}</span>
+\tarr<span class="hl opt">-&gt;</span>strs<span class="hl opt">[</span>arr<span class="hl opt">-&gt;</span>used<span class="hl opt">] =</span> str<span class="hl opt">;</span>
+\tarr<span class="hl opt">-&gt;</span>used<span class="hl opt">++;</span>
+<span class="hl opt">}</span>
+
+<span class="hl kwb">void</span> <span class="hl kwd">darr_free</span><span class="hl opt">(</span>darr<span class="hl opt">*</span> arr<span class="hl opt">) {</span>
+\t<span class="hl kwa">for</span><span class="hl opt">(</span><span class="hl kwb">int</span> i <span class="hl opt">=</span> <span class="hl num">0</span><span class="hl opt">;</span> i <span class="hl opt">&lt;</span> arr<span class="hl opt">-&gt;</span>used<span class="hl opt">;</span> i<span class="hl opt">++) {</span>
+\t\t<span class="hl kwd">free</span><span class="hl opt">(</span>arr<span class="hl opt">-&gt;</span>strs<span class="hl opt">[</span>i<span class="hl opt">]);</span>
+\t<span class="hl opt">}</span>
+\t<span class="hl kwd">free</span><span class="hl opt">(</span>arr<span class="hl opt">-&gt;</span>strs<span class="hl opt">);</span>
+\t<span class="hl kwd">free</span><span class="hl opt">(</span>arr<span class="hl opt">);</span>
+<span class="hl opt">}</span>
+
+darr<span class="hl opt">*</span> <span class="hl kwd">get_options</span><span class="hl opt">(</span><span class="hl kwb">FILE</span><span class="hl opt">*</span> file_ptr<span class="hl opt">) {</span>
+\tdarr<span class="hl opt">*</span> options <span class="hl opt">=</span> <span class="hl kwd">malloc</span><span class="hl opt">(</span><span class="hl kwa">sizeof</span><span class="hl opt">(</span>darr<span class="hl opt">));</span>
+\t<span class="hl kwa">if</span><span class="hl opt">(</span>options <span class="hl opt">==</span> NULL<span class="hl opt">)</span> <span class="hl kwd">exit</span><span class="hl opt">(</span><span class="hl num">1</span><span class="hl opt">);</span>
+\toptions<span class="hl opt">-&gt;</span>strs <span class="hl opt">=</span> <span class="hl kwd">malloc</span><span class="hl opt">(</span>D_SIZE <span class="hl opt">*</span> <span class="hl kwa">sizeof</span><span class="hl opt">(</span><span class="hl kwb">char</span><span class="hl opt">*));</span>
+\t<span class="hl kwa">if</span><span class="hl opt">(</span>options<span class="hl opt">-&gt;</span>strs <span class="hl opt">==</span> NULL<span class="hl opt">)</span> <span class="hl kwd">exit</span><span class="hl opt">(</span><span class="hl num">1</span><span class="hl opt">);</span>
+\toptions<span class="hl opt">-&gt;</span>size <span class="hl opt">=</span> D_SIZE<span class="hl opt">;</span>
+\toptions<span class="hl opt">-&gt;</span>used <span class="hl opt">=</span> <span class="hl num">0</span><span class="hl opt">;</span>
+
+\t<span class="hl kwb">char</span> chr<span class="hl opt">;</span>
+\t<span class="hl kwb">char</span><span class="hl opt">*</span> str<span class="hl opt">;</span>
+\t<span class="hl kwb">int</span> i<span class="hl opt">;</span>
+\t<span class="hl kwa">do</span> <span class="hl opt">{</span>
+\t\tstr <span class="hl opt">=</span> <span class="hl kwd">malloc</span><span class="hl opt">(</span>MAX_STR_SIZE <span class="hl opt">*</span> <span class="hl kwa">sizeof</span><span class="hl opt">(</span><span class="hl kwb">char</span><span class="hl opt">));</span>
+\t\t<span class="hl kwa">if</span><span class="hl opt">(</span>str <span class="hl opt">==</span> NULL<span class="hl opt">)</span> <span class="hl kwd">exit</span><span class="hl opt">(</span><span class="hl num">1</span><span class="hl opt">);</span>
+\t\t<span class="hl kwa">for</span><span class="hl opt">(</span>i <span class="hl opt">=</span> <span class="hl num">0</span><span class="hl opt">;</span> i <span class="hl opt">&lt;</span> MAX_STR_SIZE<span class="hl opt">;</span> i<span class="hl opt">++) {</span>
+\t\t\tchr <span class="hl opt">=</span> <span class="hl kwd">fgetc</span><span class="hl opt">(</span>file_ptr<span class="hl opt">);</span>
+\t\t\t<span class="hl kwa">if</span><span class="hl opt">(</span>chr <span class="hl opt">==</span> <span class="hl str">&apos;</span><span class="hl esc">\\n</span><span class="hl str">&apos;</span> <span class="hl opt">||</span> chr <span class="hl opt">==</span> EOF<span class="hl opt">)</span> <span class="hl kwa">break</span><span class="hl opt">;</span>
+\t\t\tstr<span class="hl opt">[</span>i<span class="hl opt">] =</span> chr<span class="hl opt">;</span>
+\t\t<span class="hl opt">}</span>
+\t\t<span class="hl kwd">darr_push</span><span class="hl opt">(</span>options<span class="hl opt">,</span> str<span class="hl opt">);</span>
+\t<span class="hl opt">}</span>
+\t<span class="hl kwa">while</span><span class="hl opt">(</span>chr <span class="hl opt">!=</span> EOF<span class="hl opt">);</span>
+
+\t<span class="hl kwa">return</span> options<span class="hl opt">;</span>
+<span class="hl opt">}</span>
+
+<span class="hl kwb">int</span> <span class="hl kwd">main</span><span class="hl opt">(</span><span class="hl kwb">int</span> argc<span class="hl opt">,</span> <span class="hl kwb">char</span><span class="hl opt">**</span> argv<span class="hl opt">) {</span>
+\t<span class="hl kwb">FILE</span><span class="hl opt">*</span> file_ptr <span class="hl opt">=</span> <span class="hl kwd">fopen</span><span class="hl opt">(</span>PATH<span class="hl opt">,</span> <span class="hl str">&quot;r&quot;</span><span class="hl opt">);</span>
+\t<span class="hl kwa">if</span><span class="hl opt">(</span>file_ptr <span class="hl opt">==</span> NULL<span class="hl opt">)</span> <span class="hl kwd">exit</span><span class="hl opt">(</span><span class="hl num">1</span><span class="hl opt">);</span>
+\tdarr<span class="hl opt">*</span> options <span class="hl opt">=</span> <span class="hl kwd">get_options</span><span class="hl opt">(</span>file_ptr<span class="hl opt">);</span>
+\t<span class="hl kwd">fclose</span><span class="hl opt">(</span>file_ptr<span class="hl opt">);</span>
+\tfile_ptr <span class="hl opt">=</span> <span class="hl kwd">fopen</span><span class="hl opt">(</span>PATH<span class="hl opt">,</span> <span class="hl str">&quot;w&quot;</span><span class="hl opt">);</span>
+\t<span class="hl kwa">if</span><span class="hl opt">(</span>file_ptr <span class="hl opt">==</span> NULL<span class="hl opt">)</span> <span class="hl kwd">exit</span><span class="hl opt">(</span><span class="hl num">1</span><span class="hl opt">);</span>
+
+\t<span class="hl kwb">int</span> chosen<span class="hl opt">;</span>
+\t<span class="hl kwa">if</span><span class="hl opt">(</span>argc <span class="hl opt">==</span> <span class="hl num">1</span><span class="hl opt">)</span>
+\t\tchosen <span class="hl opt">=</span> <span class="hl kwd">mmenu</span><span class="hl opt">(</span>options<span class="hl opt">-&gt;</span>strs<span class="hl opt">,</span> options<span class="hl opt">-&gt;</span>used<span class="hl opt">,</span> <span class="hl str">&quot;&gt; &quot;</span><span class="hl opt">);</span>
+\t<span class="hl kwa">else</span>
+\t\tchosen <span class="hl opt">=</span> <span class="hl kwd">mmenu</span><span class="hl opt">(</span>options<span class="hl opt">-&gt;</span>strs<span class="hl opt">,</span> options<span class="hl opt">-&gt;</span>used<span class="hl opt">,</span> argv<span class="hl opt">[</span><span class="hl num">1</span><span class="hl opt">]);</span>
+
+\t<span class="hl kwa">if</span><span class="hl opt">(</span>chosen <span class="hl opt">== -</span><span class="hl num">1</span><span class="hl opt">)</span>
+\t\t<span class="hl kwd">fputs</span><span class="hl opt">(</span><span class="hl str">&quot;&quot;</span><span class="hl opt">,</span> file_ptr<span class="hl opt">);</span>
+\t<span class="hl kwa">else if</span><span class="hl opt">(</span>argc <span class="hl opt">&gt;</span> <span class="hl num">2</span> <span class="hl opt">&amp;&amp;</span> argv<span class="hl opt">[</span><span class="hl num">2</span><span class="hl opt">][</span><span class="hl num">0</span><span class="hl opt">] ==</span> <span class="hl str">&apos;t&apos;</span><span class="hl opt">)</span>
+\t\t<span class="hl kwd">fprintf</span><span class="hl opt">(</span>file_ptr<span class="hl opt">,</span> <span class="hl str">&quot;%i&quot;</span><span class="hl opt">,</span> chosen<span class="hl opt">);</span>
+\t<span class="hl kwa">else</span>
+\t\t<span class="hl kwd">fputs</span><span class="hl opt">(</span>options<span class="hl opt">-&gt;</span>strs<span class="hl opt">[</span>chosen<span class="hl opt">],</span> file_ptr<span class="hl opt">);</span>
+
+\t<span class="hl kwd">darr_free</span><span class="hl opt">(</span>options<span class="hl opt">);</span>
+\t<span class="hl kwd">fclose</span><span class="hl opt">(</span>file_ptr<span class="hl opt">);</span>
+<span class="hl opt">}</span>`
 }
